4ba6cf2c655a60f8e085202c1f687f8aac7cbc3d
anil kumar <anilrocksmamu007@gmail.com>
2015-05-24 18:30:35 +0530

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/*
 var resultDiv;
 
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -20,12 +21,14 @@ function startCam() {
 	
     // Retrieve image file location from specified source
     navigator.camera.getPicture(uploadPhoto,
-                                function(message) { alert('get picture failed'); },
-                                { quality: 50, 
-                                destinationType: navigator.camera.DestinationType.FILE_URI,
-                                sourceType: navigator.camera.PictureSourceType.CAMERA,
-								MediaType: Camera.MediaType.ALLMEDIA}
-                                );
+	    function(message) { alert('get picture failed'); },
+		    { 	
+		    	quality: 50, 
+			    destinationType: navigator.camera.DestinationType.FILE_URI,
+			    sourceType: navigator.camera.PictureSourceType.CAMERA,
+				MediaType: Camera.MediaType.ALLMEDIA
+			}
+	    );
 	
 	
 }
@@ -59,6 +62,9 @@ function fail(error) {
     console.log("upload error target " + error.target);
 }
 
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 //First step check parameters mismatch and checking network connection if available call    download function
 function DownloadFile(URL, Folder_Name, File_Name) {
@@ -124,3 +130,102 @@ function filetransfer(download_link, fp) {
 		}
 	);
 }
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+function capturePhoto() {
+    sessionStorage.removeItem('imagepath');
+    // Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
+}
+
+function onPhotoDataSuccess(imageURI) { 
+        // Uncomment to view the base64 encoded image data
+        // console.log(imageData);
+
+        // Get image handle
+        //
+        var imgProfile = document.getElementById('imgProfile');
+
+        // Show the captured photo
+        // The inline CSS rules are used to resize the image
+        //
+        imgProfile.src = imageURI;
+        if(sessionStorage.isprofileimage==1){
+            getLocation();
+        }
+        movePic(imageURI);
+}
+
+// Called if something bad happens.
+// 
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+function movePic(file){ 
+    window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError); 
+} 
+
+//Callback function when the file system uri has been resolved
+function resolveOnSuccess(entry){ 
+    var d = new Date();
+    var n = d.getTime();
+    //new file name
+    var newFileName = n + ".jpg";
+    var myFolderApp = "MyAppFolder";
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {      
+    //The folder is created if doesn't exist
+    fileSys.root.getDirectory( myFolderApp,
+                    {create:true, exclusive: false},
+                    function(directory) {
+                        entry.moveTo(directory, newFileName,  successMove, resOnError);
+                    },
+                    resOnError);
+                    },
+    resOnError);
+}
+
+//Callback function when the file has been moved successfully - inserting the complete path
+function successMove(entry) {
+    //Store imagepath in session for future use
+    // like to store it in database
+    sessionStorage.setItem('imagepath', entry.fullPath);
+}
+
+function resOnError(error) {
+    alert(error.code);
+}
+*/
+
+$(document)('click', '.change-pic-btn', changePicture);
+
+changePicture = function(event) {
+    event.preventDefault();
+    console.log('changePicture');
+    if (!navigator.camera) {
+        app.showAlert("Camera API not supported", "Error");
+        return;
+    }
+    var options =   {   quality: 50,
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                        encodingType: 0     // 0=JPG 1=PNG
+                    };
+
+    navigator.camera.getPicture(
+        function(imageData) {
+            $('#image').attr('src', "data:image/jpeg;base64," + imageData);
+        },
+        function() {
+            alert('Error taking picture');
+        },
+        options);
+
+    return false;
+};
