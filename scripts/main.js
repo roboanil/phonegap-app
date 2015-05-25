0d247b88c7d9a8369785314bc59a89e6ec4d4651
anil kumar <anilrocksmamu007@gmail.com>
2015-05-25 12:15:38 +0530

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -202,69 +202,171 @@ function resOnError(error) {
     alert(error.code);
 }
 */
-$(document).ready(function(){
-// console.log("ready");
-	$(document).on('click', '.getpic', function(event){
-		event.preventDefault();
-	    console.log('changePicture');
-	    if (!navigator.camera) {
-	        if (navigator.notification) {
-	            navigator.notification.alert(
-	                message,
-	                null, // callback
-	                title,
-	                'OK' // Button label
-	            );
-	        } else {
-	            alert("hoooot");
-	        }
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value 
+
+// Wait for PhoneGap to connect with the device
+//
+document.addEventListener("deviceready",onDeviceReady,false);
+
+// PhoneGap is ready to be used!
+//
+function onDeviceReady() {
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+  // Get image handle
+  //
+  var smallImage = document.getElementById('smallImage');
+
+  // Unhide image elements
+  //
+  smallImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoFileSuccess(imageData) {
+  // // Get image handle
+  // console.log(JSON.stringify(imageData));
+  
+  // // Get image handle
+  // //
+  // var smallImage = document.getElementById('smallImage');
+
+  // // Unhide image elements
+  // //
+  // smallImage.style.display = 'block';
+
+  // // Show the captured photo
+  // // The inline CSS rules are used to resize the image
+  // //
+  // smallImage.src = imageData;
+
+  var URL = imageData;
+  var Folder_Name = "hoverxlabs";
+  var File_Name = "test.jpg";
+  DownloadFile(URL, Folder_Name, File_Name)
+
+
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+  // Uncomment to view the image file URI 
+  // console.log(imageURI);
+
+  // Get image handle
+  //
+  var largeImage = document.getElementById('largeImage');
+
+  // Unhide image elements
+  //
+  largeImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  largeImage.src = imageURI;
+}
+
+// A button will call this function
+//
+function capturePhotoWithData() {
+  // Take picture using device camera and retrieve image as base64-encoded string
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+}
+
+function capturePhotoWithFile() {
+    navigator.camera.getPicture(onPhotoFileSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI, saveToPhotoAlbum: true });
+}
+
+// A button will call this function
+//
+function getPhoto(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+    destinationType: destinationType.FILE_URI,
+    sourceType: source });
+}
+
+// Called if something bad happens.
+// 
+function onFail(message) {
+  alert('Failed because: ' + message);
+}
+
+//First step check parameters mismatch and checking network connection if available call    download function
+function DownloadFile(URL, Folder_Name, File_Name) {
+	//Parameters mismatch check
+	if (URL == null && Folder_Name == null && File_Name == null) {
+	    return;
+	}
+	else {
+	    //checking Internet connection availablity
+	    var networkState = navigator.connection.type;
+	    if (networkState == Connection.NONE) {
 	        return;
+	    } else {
+	        download(URL, Folder_Name, File_Name); //If available download function call
 	    }
-	    var options =   {   quality: 50,
-	                        destinationType: Camera.DestinationType.DATA_URL,
-	                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
-	                        encodingType: 0     // 0=JPG 1=PNG
-	                    };
+  	}
+}
 
-	    navigator.camera.getPicture(
-	        function(imageData) {
-	            $('#image').attr('src', "data:image/jpeg;base64," + imageData);
-	        },
-	        function() {
-	            alert('Error taking picture');
-	        },
-	        options);
+function download(URL, Folder_Name, File_Name) {
+//step to request a file system 
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
 
-	    return false;
-	});
-	
-/*
-	var changePicture = function(event) {
-		alert("lola");
-	    event.preventDefault();
-	    console.log('changePicture');
-	    if (!navigator.camera) {
-	        app.showAlert("Camera API not supported", "Error");
-	        return;
-	    }
-	    var options =   {   quality: 50,
-	                        destinationType: Camera.DestinationType.DATA_URL,
-	                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
-	                        encodingType: 0     // 0=JPG 1=PNG
-	                    };
+	function fileSystemSuccess(fileSystem) {
+	    var download_link = encodeURI(URL);
+	    ext = download_link.substr(download_link.lastIndexOf('.') + 1); //Get extension of URL
 
-	    navigator.camera.getPicture(
-	        function(imageData) {
-	            $('#image').attr('src', "data:image/jpeg;base64," + imageData);
-	        },
-	        function() {
-	            alert('Error taking picture');
-	        },
-	        options);
+	    var directoryEntry = fileSystem.root; // to get root path of directory
+	    directoryEntry.getDirectory(Folder_Name, { create: true, exclusive: false }, onDirectorySuccess, onDirectoryFail); // creating folder in sdcard
+	    var rootdir = fileSystem.root;
+	    var fp = rootdir.fullPath; // Returns Fulpath of local directory
 
-	    return false;
-	};
+	    fp = fp + "/" + Folder_Name + "/" + File_Name + "." + ext; // fullpath and name of the file which we want to give
+	    // download function call
+	    filetransfer(download_link, fp);
+	}
 
-*/
+	function onDirectorySuccess(parent) {
+	    // Directory created successfuly
+	}
 
-});
+	function onDirectoryFail(error) {
+	    //Error while creating directory
+	    alert("Unable to create new directory: " + error.code);
+	}
+
+	function fileSystemFail(evt) {
+		//Unable to access file system
+		alert(evt.target.error.code);
+	}
+
+}
+
+function filetransfer(download_link, fp) {
+	var fileTransfer = new FileTransfer();
+	// File download function with URL and local path
+	fileTransfer.download(download_link, fp, function (entry) {
+        alert("download complete: " + entry.fullPath);
+    }, function (error) {
+			//Download abort errors or download failed errors
+			alert("download error source " + error.source);
+			//alert("download error target " + error.target);
+			//alert("upload error code" + error.code);
+		}
+	);
+}
